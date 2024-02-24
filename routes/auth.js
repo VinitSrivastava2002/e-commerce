3619cd16e51ce2b6fc8c10ff8fe847afd588d26c
Vinit Srivastava <vinitsss1234@gmail.com>
2024-02-24 18:36:58 +0530

    login func created

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,16 @@ import CryptoJS from "crypto-js";
 
 const router = Router();
 
-/* REGISTRATION */
+/* REGISTER */
+const secretkey = "superSecretKey";
+
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
     password: CryptoJS.AES.encrypt(
       req.body.password,
-      process.env.PAS_SEC
+      process.env.SEC_KEY
     ).toString(),
   });
 
@@ -21,6 +23,35 @@ router.post("/register", async (req, res) => {
   } catch (error) {
     // console.log(error);
     res.status(500).json(error);
+  }
+});
+
+/* LOGIN */
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+    if (!user) {
+      return res.status(401).json("Wrong email");
+    }
+
+    const decryptedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.SEC_KEY
+    ).toString(CryptoJS.enc.Utf8);
+
+    if (decryptedPassword !== req.body.password) {
+      return res.status(401).json("Wrong password");
+    }
+    const { password, ...others } = user._doc;
+
+    // Password match, return user data
+    res.status(200).json(others);
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json("Internal server error");
   }
 });
 
