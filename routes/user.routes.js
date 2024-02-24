f281bc2204345c7b0948a5cb1ba244d87a88a5e9
Vinit Srivastava <vinitsss1234@gmail.com>
2024-02-24 23:53:38 +0530

    verifytoken and update users detail fixed

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,29 @@
 import { Router } from "express";
+import CryptoJS from "crypto-js";
+import { User } from "../models/user.models.js";
+import { verifyTokens, verifyTokenAndAuthorization } from "./verifyTokens.js";
 
 const router = Router();
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SEC_KEY
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json("its a error");
+  }
+});
 
 export default router;
---
+++ routes/verifyTokens.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+
+export const verifyTokens = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_KEY, (error, user) => {
+      if (error) {
+        return res.status(403).json("Invalid Token");
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
+};
+
+export const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyTokens(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Not allowed to do that");
+    }
+  });
+};
